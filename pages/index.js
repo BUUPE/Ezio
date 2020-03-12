77096e10399e028b9af30989afdac12b325f806f
Warren-Partridge <maps@perdix.org>
2020-03-12 16:14:16 -0400

    Implement Facebook login

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,38 @@
+'use strict';
+
+import React from 'react';
+import FacebookLogin from 'react-facebook-login';
 import Link from 'next/link';
 
-export default function Index() {
-  return (
-    <div>
-      <p>Welcome, assassin.</p>
-      <Link href="/leaderboard">
-        <a>Leaderboard Page</a>
-      </Link>
-    </div>
-  );
+class Index extends React.Component {
+  // This method gets called when the Facebook login gets resolved.
+  facebookAuthCallback(resp) {
+    if ("accessToken" in resp) { // Then the user must have successfully logged in.
+      alert("Successfully logged in. Welcome, " + resp.name + ".");
+    } else { // The user failed to log in.
+      alert("Login unsuccessful. Please try again.");
+    }
+
+    console.log(resp);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Welcome, assassin.</p>
+        <Link href="/leaderboard">
+          <a>Leaderboard Page</a>
+        </Link>
+
+        <FacebookLogin
+          appId="559046621373474"
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={this.facebookAuthCallback}
+        />
+      </div>
+    );
+  }
 }
+
+export default Index;
