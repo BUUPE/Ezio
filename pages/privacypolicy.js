2c4289b25f01e896c861fb72928095e891af28f2
Warren-Partridge <maps@perdix.org>
2020-03-13 14:03:30 -0400

    Add privacy policy page

---
+++ pages/privacypolicy.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <p>TODO: Privacy Policy</p>
+      <Link href="/">
+        <a>Back</a>
+      </Link>
+    </div>
+  );
+}
