05f7a81f848309f54b0b772856ae243f832c3dcd
Warren-Partridge <maps@perdix.org>
2020-05-03 00:22:00 -0400

    Import bootstrap and custom CSS

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../css/bootstrap.min.css';
+import '../css/leaderboard.css';
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -3,7 +3,13 @@ import Link from 'next/link';
 export default function Leaderboard() {
   return (
     <div>
-      <p>TODO: Public leaderboard.</p>
+      <p className="red-text">TODO: Public leaderboard.</p>
+      <div className="row">
+        <div className="col-sm-4">Ezio</div>
+        <div className="col-sm-4">Auditore</div>
+        <div className="col-sm-4">da Firenze</div>
+      </div>
+
       <Link href="/">
         <a>Back</a>
       </Link>
