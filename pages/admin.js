bcc17f9c93dbbee78a25fdcbd1f1528864d77227
Yuanming Chai <yaunmingchai@gmail.com>
2020-05-12 17:28:08 -0400

    admin

--- pages/admin.js
+++ pages/admin.js
@@ -1,7 +1,11 @@
 import Link from 'next/link';
-
 import React from "react";
+
 import {
+  Modal,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
   Card,
   Button,
   CardHeader,
@@ -77,10 +81,31 @@ class Admin extends React.Component {
   handleReportButtonClicked() {
     console.log("hello I was clicked");
     this.setState({
-      formIsHidden: false
+      formIsHidden: false,
+      smShow: true
     });
   }
-
+  
+  // MyVerticallyCenteredModal() {
+  //   const [smShow, setSmShow] = useState(false);
+  //   return (
+  //     <>
+  //     <Modal
+  //       size="sm"
+  //       show={smShow}
+  //       onHide={() => setSmShow(false)}
+  //       aria-labelledby="example-modal-sizes-title-sm"
+  //     >
+  //       <Modal.Header closeButton>
+  //         <Modal.Title id="example-modal-sizes-title-sm">
+  //           Small Modal
+  //         </Modal.Title>
+  //       </Modal.Header>
+  //       <Modal.Body>...</Modal.Body>
+  //     </Modal>
+  //     </>
+  //   )
+  // }
     render() {
       return (
         <>
@@ -146,27 +171,48 @@ class Admin extends React.Component {
           </Link>
         </div>
 
+        <Modal isOpen={true}>
+        <ModalHeader>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" >Do Something</Button>{' '}
+          <Button color="secondary" >Cancel</Button>
+        </ModalFooter>
+      </Modal>
 
-      <form class="form-popup" id="myForm" hidden={this.state.formIsHidden} >
-      <div class="form-check">
-            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked></input>
-            <label class="form-check-label" for="gridRadios1">
-              Alive
-            </label>
-          </div>
-          <div class="form-check">
-            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2"></input>
-            <label class="form-check-label" for="gridRadios2">
-              Killed
-            </label>
-          </div>
-          <div class="form-check">
-            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled></input>
-            <label class="form-check-label" for="gridRadios3">
-              Reported
-            </label>
-          </div>
-      </form>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <form class="form-popup" id="myForm" hidden={this.state.formIsHidden}>
+                <div class="form-check">
+                      <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="alive" checked></input>
+                      <label class="form-check-label" for="gridRadios1">
+                        Alive
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="killed"></input>
+                      <label class="form-check-label" for="gridRadios2">
+                        Killed
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="reported" disabled></input>
+                      <label class="form-check-label" for="gridRadios3">
+                        Reported
+                      </label>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+      </div>
       </>
       );
     }
